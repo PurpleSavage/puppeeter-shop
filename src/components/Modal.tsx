beaaf747087543f397de3b5da7b99f607cd9e5d6
PurpleSavage <jzuritapalomino@gmail.com>
2024-02-19 11:46:54 -0500

    cambiar modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,6 @@
 import { MdOutlineFileDownloadDone } from "react-icons/md";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
 import { Link } from "react-router-dom"
 import { useModalStore } from "../stores/cart/modal.store";
 const Modal = () => {
@@ -11,10 +13,15 @@ const Modal = () => {
   return (
     <div className="fixed bottom-0 right-0 left-0 top-0 flex items-center justify-center bg-slate-200/70 z-50">
         <section className="w-full md:w-1/3 shadow-2xl bg-slate-200 rounded-lg p-4 space-y-2">
-            <h3 className="flex items-center gap-2 text-lg font-bold">
-                <span className="text-green-600 "><MdOutlineFileDownloadDone size={20} /></span>
-                Producto agregado correctamente
-            </h3>
+            <div className="flex justify-between ">
+                <h3 className="flex items-center gap-2 text-lg font-bold">
+                    <span className="text-green-600 "><MdOutlineFileDownloadDone size={20} /></span>
+                    Producto agregado
+                </h3>
+                <button onClick={handleClick}>
+                    <IoIosCloseCircleOutline size={25}/>
+                </button>
+            </div>
             <p className="mt-4">Se agregó un producto al carrito!</p>
             <div className="0 flex justify-between py-2" >
                 <Link 
@@ -24,12 +31,7 @@ const Modal = () => {
                     text-white items-center px-2 h-[30px] text-md flex ">
                     Ir al carrito
                 </Link>
-                <button 
-                    className="bg-red-600 hover:bg-red-700 px-2  text-white rounded-md  text-md"
-                    onClick={handleClick}
-                >
-                    cerrar modal
-                </button>
+                
             </div>
         </section>
     </div>
